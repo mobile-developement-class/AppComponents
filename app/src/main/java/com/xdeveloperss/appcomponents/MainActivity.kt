8aa53842e657d9867c62d6185f18ddbb6b6e571b
AnasSharif <anassharif1992@gmail.com>
2022-11-23 11:46:15 +0500

    Add Activity lifecycle

--- app/src/main/java/com/xdeveloperss/appcomponents/MainActivity.kt
+++ app/src/main/java/com/xdeveloperss/appcomponents/MainActivity.kt
@@ -2,10 +2,26 @@ package com.xdeveloperss.appcomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    companion object{
+        const val TAG = "MainActivity"
     }
 }
